870788e8f82896436d2bafb607760f77f0954c61
amber_sleepeanuty <13426014388@163.com>
2017-03-10 10:09:55 +0800

    Initial commit: gradle configration has settled down.

---
+++ library/src/main/java/com/codo/amber_sleepeanuty/library/libs.java
@@ -0,0 +1,13 @@
+package com.codo.amber_sleepeanuty.library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class libs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_libs);
+    }
+}
