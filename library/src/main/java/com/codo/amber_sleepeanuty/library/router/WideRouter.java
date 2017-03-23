45bd46c46c8c52ce9b190400228e220a4afe3e5c
amber_sleepeanuty <13426014388@163.com>
2017-03-23 14:42:16 +0800

    111

--- library/src/main/java/com/codo/amber_sleepeanuty/library/router/WideRouter.java
+++ library/src/main/java/com/codo/amber_sleepeanuty/library/router/WideRouter.java
@@ -1,5 +1,18 @@
 package com.codo.amber_sleepeanuty.library.router;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import com.codo.amber_sleepeanuty.library.CodoApplication;
+import com.codo.amber_sleepeanuty.library.ILocalRouterAIDL;
+import com.codo.amber_sleepeanuty.library.service.LocalConnectService;
+import com.codo.amber_sleepeanuty.library.util.ProcessNameUtil;
+import com.codo.amber_sleepeanuty.library.wrapper.LocalServiceWrapper;
+
 import java.util.HashMap;
 
 /**
@@ -7,14 +20,84 @@ import java.util.HashMap;
  */
 
 public class WideRouter {
-    //存已注册的需多进程module的localrouter
-    private HashMap<String,LocalRouter> mRouterMap;
 
-    public WideRouter() {
+    private CodoApplication context;
+    private String processName = "com.codo.amber_sleepeanuty.widerouter";
+    private WideRouter tempInstance;
+    private HashMap<String,LocalServiceWrapper> serviceWrapperMap;
+    private HashMap<String,ILocalRouterAIDL> LocalRouterAIDLMap;
+    private HashMap<String,ServiceConnection> LocalConnectServiceMap;
+
+    private WideRouter(CodoApplication context){
+        this.context = context;
+        String requestProcessName = ProcessNameUtil.getProcessName(context,ProcessNameUtil.getMyProcessId());
+        if(!processName.equals(requestProcessName)){
+            throw new RuntimeException("you can't intial widerRouter in your current process");
+        }
+        serviceWrapperMap = new HashMap<>();
 
     }
 
-    public registerLocalConnectService(String processName){
+    public WideRouter getInstence(CodoApplication context){
+        if(null!=context){
+            tempInstance = new WideRouter(context);
+        }
+        return tempInstance;
+    }
+
+
+    public void registerLocalConnectService(String domain,Class<? extends LocalConnectService> register ){
+        if(null==serviceWrapperMap) {
+            serviceWrapperMap = new HashMap<>();
+        }
+        LocalServiceWrapper serviceWrapper = new LocalServiceWrapper(register);
+        serviceWrapperMap.put(domain, serviceWrapper);
+    }
+
+    public Class<? extends LocalConnectService> fetchLocalConnectService(String domain){
+        if(null!=serviceWrapperMap){
+            LocalServiceWrapper wrapper = serviceWrapperMap.get(domain);
+            if(null!=wrapper.targetService){
+                return wrapper.targetService;
+            }
+        }
+    }
+
+    public boolean connectLocalRouter(final String domain){
+        boolean result = false;
+
+        LocalServiceWrapper wrapper = serviceWrapperMap.get(domain);
+        if(null == wrapper){
+            return result;
+        }
+
+        Class<? extends LocalConnectService> targetSerive = wrapper.targetService;
+        if(null == targetSerive){
+            return result;
+        }
+
+        Intent bindIntent = new Intent(context,targetSerive);
+        Bundle bundle= new Bundle();
+        bindIntent.putExtras(bundle);
+
+        final ServiceConnection connection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                ILocalRouterAIDL mLocalRouter = ILocalRouterAIDL.Stub.asInterface(service);
+                ILocalRouterAIDL temp = LocalRouterAIDLMap.get(domain);
+                if(null==temp){
+                    LocalRouterAIDLMap.put(domain,mLocalRouter);
+                    LocalConnectServiceMap.put(domain,this);
+                }
+                mLocalRouter.
+
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        }
 
     }
 }
