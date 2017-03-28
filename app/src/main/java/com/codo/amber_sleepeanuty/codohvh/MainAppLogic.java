28fcd7f02d358a9e3f9ce973771db958fd94db91
amber_sleepeanuty <13426014388@163.com>
2017-03-28 17:06:41 +0800

    3.28

---
+++ app/src/main/java/com/codo/amber_sleepeanuty/codohvh/MainAppLogic.java
@@ -0,0 +1,16 @@
+package com.codo.amber_sleepeanuty.codohvh;
+
+import com.codo.amber_sleepeanuty.library.base.BaseAppLogic;
+import com.codo.amber_sleepeanuty.library.router.LocalRouter;
+
+/**
+ * Created by amber_sleepeanuty on 2017/3/28.
+ */
+
+public class MainAppLogic extends BaseAppLogic {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LocalRouter.getInstance(mApplication).registerProvider(mApplication,"Main",new MainProvider());
+    }
+}
---
+++ app/src/main/java/com/codo/amber_sleepeanuty/codohvh/MainProvider.java
@@ -0,0 +1,18 @@
+package com.codo.amber_sleepeanuty.codohvh;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.codo.amber_sleepeanuty.library.base.BaseAction;
+import com.codo.amber_sleepeanuty.library.base.BaseProvider;
+
+/**
+ * Created by amber_sleepeanuty on 2017/3/28.
+ */
+
+public class MainProvider extends BaseProvider {
+    @Override
+    public void registerAction(Context context, @NonNull Class<? extends BaseAction> action) {
+
+    }
+}
