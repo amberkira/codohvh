f87172416282c89e0b42ee6e47fa90609ed31cc3
amber_sleepeanuty <13426014388@163.com>
2017-04-09 17:15:54 +0800

    4.9 bmob引入登陆验证

--- module_login/src/main/java/com/codo/amber_sleepeanuty/module_login/SignupActivity.java
+++ module_login/src/main/java/com/codo/amber_sleepeanuty/module_login/SignupActivity.java
@@ -11,5 +11,6 @@ public class SignupActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
     }
 }
